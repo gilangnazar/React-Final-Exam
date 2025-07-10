d3fc88e51cd88c79789e30188537b51a9ff7bf69
gilangnazar <gilangnazar55@gmail.com>
2025-07-10 16:27:30 +0700

    menambahkan PUT dan DELETE ke api /departments

--- backend/routes/departments.js
+++ backend/routes/departments.js
@@ -22,13 +22,46 @@ router.get('/departments', async (req, res) => {
 router.post('/departments', async (req, res) => {
   try {
     const { name, description } = req.body;
-    if (!name || !description) return res.status(400).json({ msg: 'Missing data' });
+    if (!name || !description) return res.status(400).json({ msg: 'Incomplete data' });
 
     await db.execute('INSERT INTO departments(name, description) VALUES(?,?)', [name, description]);
 
     return res.status(201).json({ msg: 'Departemen baru berhasil dibuat' });
   } catch (error) {
     return res.status(500).json({ msg: 'Server error', err: error });
+  }
+});
+
+router.put('/departments/:department_id', async (req, res) => {
+  try {
+    const { name, description } = req.body;
+    const { department_id } = req.params;
+
+    if (!name || !description) return res.status(400).json({ msg: 'Incomplete data' });
+    if (!department_id) return res.status(400).json({ msg: 'Department ID Is Invalid or Missing' });
+
+    await db.execute('UPDATE departments SET name = ?, description = ? WHERE department_id = ?', [
+      name,
+      description,
+      department_id,
+    ]);
+
+    return res.json({ msg: 'Departemen berhasil Diedit' });
+  } catch (error) {
+    return res.status(500).json({ msg: 'Server error', err: error.message });
+  }
+});
+
+router.delete('/departments/:department_id', async (req, res) => {
+  try {
+    const { department_id } = req.params;
+    if (!department_id) return res.status(400).json({ msg: 'Department ID Is Invalid or Missing' });
+
+    await db.execute('UPDATE departments SET deleted_at = NOW() WHERE department_id = ?', [department_id]);
+
+    return res.json({ msg: 'Departemen berhasil Terhapus (Soft delete)' });
+  } catch (error) {
+    return res.status(500).json({ msg: 'Server error', err: error.message });
   }
 });
 
