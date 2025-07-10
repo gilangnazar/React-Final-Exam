b682cce2a2749a3d2bcf1c0e61fd37203dee08ef
gilangnazar <gilangnazar55@gmail.com>
2025-07-10 16:11:02 +0700

    menambahkan route POST untuk api /departments

--- backend/routes/departments.js
+++ backend/routes/departments.js
@@ -19,4 +19,17 @@ router.get('/departments', async (req, res) => {
   }
 });
 
+router.post('/departments', async (req, res) => {
+  try {
+    const { name, description } = req.body;
+    if (!name || !description) return res.status(400).json({ msg: 'Missing data' });
+
+    await db.execute('INSERT INTO departments(name, description) VALUES(?,?)', [name, description]);
+
+    return res.status(201).json({ msg: 'Departemen baru berhasil dibuat' });
+  } catch (error) {
+    return res.status(500).json({ msg: 'Server error', err: error });
+  }
+});
+
 module.exports = router;
