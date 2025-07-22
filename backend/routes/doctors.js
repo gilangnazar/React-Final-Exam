09aacc928810ea676052771b6dc9a28ab847a73e
gilangnazar <gilangnazar55@gmail.com>
2025-07-22 16:45:16 +0700

    uncomment doctor crud

--- backend/routes/doctors.js
+++ backend/routes/doctors.js
@@ -10,96 +10,96 @@ router.get('/doctors/:user_id/appointments', appointmentsController.doctorFetchA
 router.post('/doctors/:user_id/examinations', examinationsController.createExamination);
 router.post('/doctors/prescriptions', prescriptionsController.createPrescription);
 
-// router.get('/doctors', async (req, res) => {
-//   try {
-//     const query = `SELECT
-// doc.doctor_id,
-// u.full_name AS doctor_name,
-// dep.name AS department_name,
-// doc.specialization,
-// doc.user_id
-// FROM doctors doc
-// JOIN departments dep ON doc.department_id = dep.department_id AND dep.deleted_at IS NULL
-// JOIN users u ON doc.user_id = u.user_id AND u.deleted_at IS NULL
-// WHERE doc.deleted_at IS NULL
-// ORDER BY doc.doctor_id DESC;`;
+router.get('/doctors', async (req, res) => {
+  try {
+    const query = `SELECT
+doc.doctor_id,
+u.full_name AS doctor_name,
+dep.name AS department_name,
+doc.specialization,
+doc.user_id
+FROM doctors doc
+JOIN departments dep ON doc.department_id = dep.department_id AND dep.deleted_at IS NULL
+JOIN users u ON doc.user_id = u.user_id AND u.deleted_at IS NULL
+WHERE doc.deleted_at IS NULL
+ORDER BY doc.doctor_id DESC;`;
 
-//     const [data] = await db.execute(query);
+    const [data] = await db.execute(query);
 
-//     res.status(200).json({
-//       msg: 'Data berhasil diambil',
-//       data: data,
-//     });
-//   } catch (error) {
-//     res.status(500).json({
-//       msg: 'Server error',
-//       err: error,
-//     });
-//   }
-// });
+    res.status(200).json({
+      msg: 'Data berhasil diambil',
+      data: data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      msg: 'Server error',
+      err: error,
+    });
+  }
+});
 
-// router.post('/doctors', async (req, res) => {
-//   try {
-//     const { user_id, department_id, specialization } = req.body;
-//     if (!user_id || !department_id || !specialization)
-//       return res.status(400).json({ msg: 'Incomplete data' });
+router.post('/doctors', async (req, res) => {
+  try {
+    const { user_id, department_id, specialization } = req.body;
+    if (!user_id || !department_id || !specialization)
+      return res.status(400).json({ msg: 'Incomplete data' });
 
-//     await db.execute('INSERT INTO doctors(user_id, department_id, specialization) VALUES(?,?,?)', [
-//       user_id,
-//       department_id,
-//       specialization,
-//     ]);
+    await db.execute('INSERT INTO doctors(user_id, department_id, specialization) VALUES(?,?,?)', [
+      user_id,
+      department_id,
+      specialization,
+    ]);
 
-//     return res.status(201).json({ msg: 'Data Dokter baru berhasil dibuat' });
-//   } catch (error) {
-//     return res.status(500).json({ msg: 'Server error', err: error });
-//   }
-// });
+    return res.status(201).json({ msg: 'Data Dokter baru berhasil dibuat' });
+  } catch (error) {
+    return res.status(500).json({ msg: 'Server error', err: error });
+  }
+});
 
-// router.put('/doctors/:doctor_id', async (req, res) => {
-//   try {
-//     const { user_id, department_id, specialization } = req.body;
-//     const { doctor_id } = req.params;
+router.put('/doctors/:doctor_id', async (req, res) => {
+  try {
+    const { user_id, department_id, specialization } = req.body;
+    const { doctor_id } = req.params;
 
-//     if (!user_id || !department_id || !specialization)
-//       return res.status(400).json({ msg: 'Incomplete data' });
-//     if (!doctor_id) return res.status(400).json({ msg: 'Doctor ID Is Invalid or Missing' });
+    if (!user_id || !department_id || !specialization)
+      return res.status(400).json({ msg: 'Incomplete data' });
+    if (!doctor_id) return res.status(400).json({ msg: 'Doctor ID Is Invalid or Missing' });
 
-//     await db.execute(
-//       'UPDATE doctors SET user_id = ?, department_id = ?, specialization = ? WHERE doctor_id = ?',
-//       [user_id, department_id, specialization, doctor_id]
-//     );
+    await db.execute(
+      'UPDATE doctors SET user_id = ?, department_id = ?, specialization = ? WHERE doctor_id = ?',
+      [user_id, department_id, specialization, doctor_id]
+    );
 
-//     return res.json({ msg: 'Data Dokter berhasil Diedit' });
-//   } catch (error) {
-//     return res.status(500).json({ msg: 'Server error', err: error.message });
-//   }
-// });
+    return res.json({ msg: 'Data Dokter berhasil Diedit' });
+  } catch (error) {
+    return res.status(500).json({ msg: 'Server error', err: error.message });
+  }
+});
 
-// router.delete('/doctors/:doctor_id', async (req, res) => {
-//   try {
-//     const { doctor_id } = req.params;
-//     if (!doctor_id) return res.status(400).json({ msg: 'Doctor ID Is Invalid or Missing' });
+router.delete('/doctors/:doctor_id', async (req, res) => {
+  try {
+    const { doctor_id } = req.params;
+    if (!doctor_id) return res.status(400).json({ msg: 'Doctor ID Is Invalid or Missing' });
 
-//     await db.execute('UPDATE doctors SET deleted_at = NOW() WHERE doctor_id = ?', [doctor_id]);
+    await db.execute('UPDATE doctors SET deleted_at = NOW() WHERE doctor_id = ?', [doctor_id]);
 
-//     return res.json({ msg: 'Dokter berhasil Terhapus (Soft delete)' });
-//   } catch (error) {
-//     return res.status(500).json({ msg: 'Server error', err: error.message });
-//   }
-// });
+    return res.json({ msg: 'Dokter berhasil Terhapus (Soft delete)' });
+  } catch (error) {
+    return res.status(500).json({ msg: 'Server error', err: error.message });
+  }
+});
 
-// router.put('/doctors/:doctor_id/restore', async (req, res) => {
-//   try {
-//     const { doctor_id } = req.params;
-//     if (!doctor_id) return res.status(400).json({ msg: 'Doctor ID Is Invalid or Missing' });
+router.put('/doctors/:doctor_id/restore', async (req, res) => {
+  try {
+    const { doctor_id } = req.params;
+    if (!doctor_id) return res.status(400).json({ msg: 'Doctor ID Is Invalid or Missing' });
 
-//     await db.execute('UPDATE doctors SET deleted_at = NULL WHERE doctor_id = ?', [doctor_id]);
+    await db.execute('UPDATE doctors SET deleted_at = NULL WHERE doctor_id = ?', [doctor_id]);
 
-//     return res.json({ msg: 'Dokter berhasil direstore' });
-//   } catch (error) {
-//     return res.status(500).json({ msg: 'Server error', err: error.message });
-//   }
-// });
+    return res.json({ msg: 'Dokter berhasil direstore' });
+  } catch (error) {
+    return res.status(500).json({ msg: 'Server error', err: error.message });
+  }
+});
 
 module.exports = router;
